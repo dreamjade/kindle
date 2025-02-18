ac6a90f2bc2b66827c12d349d00ca7f85686c885
YU-CHIA LIN <chc860827@gmail.com>
2025-02-18 00:40:59 -0700

    Update oled.js

--- oled.js
+++ oled.js
@@ -5,8 +5,8 @@ function setupOLEDProtection() {
   // Function to move the app slightly
   function moveApp() {
     if (body.classList.contains('dark-mode')) {
-      const randomX = Math.random() * 10 - 5; // Random value between -5 and 5 pixels
-      const randomY = Math.random() * 10 - 5; // Random value between -5 and 5 pixels
+      const randomX = Math.random() * 6 - 3; // Random value between -3 and 3 pixels
+      const randomY = Math.random() * 6 - 3; // Random value between -3 and 3 pixels
       appElement.style.transform = `translate(calc(-50% + ${randomX}px), calc(-50% + ${randomY}px))`;
     } else {
       appElement.style.transform = 'translate(-50%, -50%)'; // Reset to center in light mode
@@ -14,7 +14,7 @@ function setupOLEDProtection() {
   }
 
   // Move the app every 5 seconds (adjust interval as needed)
-  setInterval(moveApp, 5000);
+  setInterval(moveApp, 300000);
 }
 
 // Call the setup function when the script is loaded
